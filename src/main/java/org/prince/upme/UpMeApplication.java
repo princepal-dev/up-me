8c36f11aaca8529bcf4696d8d874dc229a7e01ba
Prince Pal <rishabpal2000@gmail.com>
2026-06-14 12:51:42 +0530

    added base files & init project

---
+++ src/main/java/org/prince/upme/UpMeApplication.java
@@ -0,0 +1,13 @@
+package org.prince.upme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UpMeApplication.class, args);
+    }
+
+}
